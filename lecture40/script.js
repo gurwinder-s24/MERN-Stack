2fb2ff1180220d1b6180e0f88a103ab94107d2ba
GURWINDER SINGH <gaurav98498@gmail.com>
2025-10-31 21:19:50 +0530

    lecture40

---
+++ lecture40/script.js
@@ -0,0 +1,64 @@
+// Variables and Data Types in JavaScript
+var age = 10;
+if(true){
+    console.log();
+    console.log(age);
+}
+// var is available as global variable
+
+function print(){
+    var num = 20;
+    console.log(num);
+}
+print();
+// console.log(num);
+// will give error because 
+// here var is function scoped
+
+var a = 30;
+console.log(a);
+var a = 40;
+console.log(a);
+// declaration of var multiple times is 
+// possible but not recommended 
+// as it can lead to debugging issues
+
+
+
+
+let b = 50;
+console.log(b);
+
+{
+    let c = 50;
+}
+// console.log(c);
+// will give error because
+// let is block scoped
+
+// let b = 55;
+// will give error
+// re-declaration not possible
+
+let d = 60;
+d = "name";
+d = true;
+d = null;
+console.log(d);
+// # dynamic typed language
+
+
+
+
+const pi = 3.14;
+console.log(pi);
+// pi = 3.145;
+// re-assignment not possible
+
+// const pi;
+// re-declaration not possible
+
+// const pi = 3.145;
+// re-definition not possible
+
+
