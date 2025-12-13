f569566c13eeccb94f0c61b9db4033410911320e
GURWINDER SINGH <gaurav98498@gmail.com>
2025-12-13 22:38:48 +0530

    lecture48

---
+++ lecture48/in-built_Objects.js
@@ -0,0 +1,28 @@
+// Math Object
+// console.log(Math.PI);
+// console.log(Math.random(16));
+// console.log(Math.floor(1.5));
+// console.log(Math.round(1.4));
+// console.log(Math.round(1.5));
+// console.log(Math.ceil(1.4));
+// console.log(Math.abs(-3));
+// console.log(Math.sqrt(16));
+// console.log(Math.min(10, 20, 30,999,4));
+// console.log(Math.max(10, 20, 30,999,4));
+// console.log(Math.pow(2, 10));
+
+
+// Date Object
+let now = new Date();
+console.log(now);
+now.setFullYear(2013);
+console.log(now.getFullYear());
+console.log(now.getMonth() + 1);
+console.log(now.getDate());
+console.log(now.getDay());
+
+
+// let date = new Date('March 25 2006 07:30');
+let date = new Date(2006, 2, 25, 7, 30);
+console.log();
+console.log(date);
