ed8eab9f39474eb9ccfd3ad64b5961c158e4d0de
GURWINDER SINGH <gaurav98498@gmail.com>
2025-12-14 17:02:39 +0530

    lecture50

---
+++ lecture50/errorHandling.js
@@ -0,0 +1,36 @@
+
+// Compile time error
+// Syntax error
+// console.log(1;
+
+// Runtime error
+// Reference error
+// console.log(x);
+
+
+// try{
+//     // console.log(x);
+//     let result = 10 / 0;
+//     console.log();
+// }
+// catch(e){
+//     console.log("Error: " + e.message);
+// }
+// finally{
+//     console.log("Execution completed.");
+// }
+
+
+// Throwing custom error
+// try{
+//     console.log(x);
+// }
+// catch(e){
+//     throw new Error("First declare then print.");
+// }
+
+// let errorCode = 100;
+// if(errorCode === 100){
+//     throw new Error("Invalid Json");
+// }
+
