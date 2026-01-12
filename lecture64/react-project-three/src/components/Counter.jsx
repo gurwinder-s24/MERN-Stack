d0e3a63047fa0b2cf89284d4c3241ac5de70e7ba
GURWINDER SINGH <gaurav98498@gmail.com>
2026-01-12 23:09:18 +0530

    lecture64

---
+++ lecture64/react-project-three/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import React, { useState } from 'react'
+import './Counter.css'
+
+const Counter = () => {
+    // const [count, setCount] = React.useState(0);
+    const [count, setCount] = useState(0);
+
+  return (
+    <div className="counter-container">
+      <p id='para'>
+        You have clicked the button {count} times.
+      </p>
+      <button id='btn' onClick={()=> {setCount(count+1)}} >Counter++</button>
+    </div>
+  )
+}
+
+export default Counter
